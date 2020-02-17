18e82fac000cb45522a589d1af2134c3abec870f
Zoubeir M <44193946+Zoubir78@users.noreply.github.com>
2020-02-17 12:10:44 +0100

    Add files via upload

---
+++ test-dev/js/burger-menu.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+
+  $('.nav-icon').on('click', function(e){
+  $(this).toggleClass('open');	
+  $('.mobile_menu').toggleClass('visible');
+  });
+	
+});  
---
+++ test-dev/js/test.js
@@ -0,0 +1,9 @@
+
+// PARALLAX SCROLLING
+
+$(document).ready(function() {
+  $('.parallax-window').parallax({imageSrc: './img/ad.jpg'});
+  $('.parallax-window-two').parallax({imageSrc: './img/r2d2_c3p0.jpg'});
+  $('.parallax-window-three').parallax({imageSrc: './img/kiloren.jpg'});
+  $('.parallax-window-four').parallax({imageSrc: './img/stormtroopers.jpg'});  
+});
